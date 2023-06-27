689285e196540f913fa1dabfd447a0d1c8c8444a
Yashaswini Shivathaya <evilgeniusprevails@gmail.com>
2023-06-27 11:24:59 +0530

    added logo

--- app/header.js
+++ app/header.js
@@ -1,27 +1,26 @@
 "use client";
 import { useSession } from "next-auth/react";
 import { useState } from "react";
+import Image from "next/image";
 
 export default function HeaderPage() {
   const [show, setShow] = useState(false);
   const { data: s, _ } = useSession();
   return (
     <div className="bg-transparent sm:bg-latte fixed z-50 w-full sm:h-[10vh] text-jet flex justify-between items-start sm:items-center md:px-8 p-4">
-      {s?.user?.email ? (
-        <a
-          href={`/${s.user.email}`}
-          className="xs:text-4xl text-2xl hover:bg-coral hover:bg-opacity-25 ease-in duration-200 px-4 py-2 rounded-full"
-        >
-          Pomodoro
-        </a>
-      ) : (
-        <a
-          href="/"
-          className="xs:text-4xl text-2xl hover:bg-coral hover:bg-opacity-25 ease-in duration-200 px-4 py-2 rounded-full"
-        >
-          Pomodoro
-        </a>
-      )}
+      <span className="flex justify-center gap-2 hover:bg-coral hover:bg-opacity-25 ease-in duration-200 px-4 py-2 rounded-full">
+        <Image src="/pomodoro.svg" width={25} height={25} alt="Logo" />
+
+        {s?.user?.email ? (
+          <a href={`/${s.user.email}`} className="sm:text-4xl text-2xl ">
+            Pomodoro
+          </a>
+        ) : (
+          <a href="/" className="sm:text-4xl text-2xl ">
+            Pomodoro
+          </a>
+        )}
+      </span>
       {s?.user ? (
         <span className="flex sm:flex-row flex-col sm:justify-center justify-start sm:text-base text-xs items-center gap-2">
           <img
