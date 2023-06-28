9d8821f8d1c25e44e5738d8b8bbae334056daa62
Yashaswini Shivathaya <evilgeniusprevails@gmail.com>
2023-06-28 11:04:13 +0530

    ..

--- app/page.js
+++ app/page.js
@@ -23,7 +23,9 @@ export default function LoginPage() {
         </>
       ) : (
         <a
-          href="/api/auth/signin"
+          href={`/api/auth/signin?callbackUrl=${encodeURIComponent(
+            "https://taskpomodoros.vercel.app"
+          )}`}
           className="border-2 font-bold border-jet duration-200 ease-in w-fit px-2 py-1 rounded-sm hover:bg-jet hover:text-latte cursor-pointer"
         >
           SIGNIN
